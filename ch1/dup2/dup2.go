f8427e56de446fa166683e507a2ed156b9cc187e
Wes <wesleyayer92@gmail.com>
2020-11-25 14:50:25 -0500

    add filename print to dup2

--- ch1/dup2/dup2.go
+++ ch1/dup2/dup2.go
@@ -11,30 +11,26 @@ import (
 func main() {
 	counts := make(map[string]int)
 	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, file := range files {
-			f, err := os.Open(file)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			continue
 		}
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%s\t%d\n", line, n)
-		}
+		countLines(file, f, counts)
+		f.Close()
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(fname string, f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%s\t%d in file %s\n", line, n, fname)
+		}
 	}
 }
 
